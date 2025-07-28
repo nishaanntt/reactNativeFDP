9abf0c79456f200064b58c6c23a200bcceb38017
nishaanntt <nparashar1998@gmail.com>
2025-07-29 01:27:18 +0530

    safeareaview

--- App.js
+++ App.js
@@ -1,62 +1,34 @@
-import {
-	Dimensions,
-	StyleSheet,
-	Text,
-	View,
-	useWindowDimensions,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-	// const windowWidth = useWindowDimensions().width;
-	// const windowHeight = useWindowDimensions().height;
 	return (
-		<View style={styles.container}>
-			<View
-				style={[
-					styles.box,
-					// {
-					// 	width: windowWidth > 500 ? '70%' : '90%',
-					// 	height: windowHeight > 600 ? '60%' : '90%',
-					// },
-				]}>
-				<Text
-					style={[
-						styles.text,
-						// {
-						// 	fontSize: windowWidth > 500 ? 50 : 24,
-						// },
-					]}>
-					Welcome
-				</Text>
+		<SafeAreaView style={styles.safeContainer}>
+			<View style={styles.container}>
+				<View style={styles.box}>
+					<Text style={styles.text}>Welcome</Text>
+				</View>
 			</View>
-		</View>
+		</SafeAreaView>
 	);
 };
 
 export default App;
 
-// const windowWidth = Dimensions.get('window').width;
-// const windowHeight = Dimensions.get('window').height;
-// Drawback of Dimensions API - The dimensions change with orientation and the API does not dynamically update with refresh
-
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: 'plum',
-		alignItems: 'center',
-		justifyContent: 'center',
 	},
 	box: {
-		width: 300,
-		// width: windowWidth > 500 ? '70%' : '90%',
-		height: 300,
-		// height: windowHeight > 600 ? '60%' : '90%',
-		backgroundColor: 'lightblue',
-		alignItems: 'center',
-		justifyContent: 'center',
+		padding: 20,
 	},
 	text: {
 		fontSize: 24,
-		// fontSize: windowWidth > 500 ? 50 : 24,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	safeContainer: {
+		flex: 1,
+		backgroundColor: 'plum',
 	},
 });
