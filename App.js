6dbe6271634336a193ed00703c1e12c2fcbafb9e
nishaanntt <nparashar1998@gmail.com>
2025-07-29 03:02:34 +0530

    flatlist

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import {
+	FlatList,
 	SafeAreaView,
 	ScrollView,
 	StatusBar,
@@ -12,8 +13,11 @@ import pokemonList from './data.json';
 const App = () => {
 	return (
 		<SafeAreaView style={styles.container}>
-			<ScrollView style={styles.scrollView}>
+			{/* *** RENDERING LISTS WITH MAP *** */}
+
+			{/* <ScrollView style={styles.scrollView}>
 				{pokemonList.map(pokemon => {
+        console.log(pokemon.id)
 					return (
 						<View style={styles.card} key={pokemon.id}>
 							<Text style={styles.cardText}>{pokemon.name}</Text>
@@ -21,7 +25,34 @@ const App = () => {
 						</View>
 					);
 				})}
-			</ScrollView>
+			</ScrollView> */}
+
+			{/* *** FLATLIST *** */}
+
+			{/* <View style={styles.scrollView}>
+				<FlatList
+					data={pokemonList}
+					renderItem={({ item }) => {
+						console.log(item.id);
+						return (
+							<View style={styles.card} key={item.id}>
+								<Text style={styles.cardText}>{item.name}</Text>
+								<Text style={styles.cardText}>{item.type}</Text>
+							</View>
+						);
+					}}
+					// keyExtractor={(item, index) => item.id.toString()}
+					// ItemSeparatorComponent={<View style={{ height: 16 }} />}
+					// horizontal={true}
+					// ListEmptyComponent={<Text>No Items Found</Text>}
+					// ListHeaderComponent={
+					// 	<Text style={styles.headerText}>Pokemon List</Text>
+					// }
+					// ListFooterComponent={
+					// 	<Text style={styles.footerText}>End of List</Text>
+					// }
+				/>
+			</View> */}
 		</SafeAreaView>
 	);
 };
@@ -46,5 +77,15 @@ const styles = StyleSheet.create({
 	},
 	cardText: {
 		fontSize: 24,
+	},
+	headerText: {
+		fontSize: 24,
+		textAlign: 'center',
+		marginBottom: 12,
+	},
+	footerText: {
+		fontSize: 24,
+		textAlign: 'center',
+		marginTop: 12,
 	},
 });
