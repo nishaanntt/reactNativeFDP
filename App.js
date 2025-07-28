1795b015d5b714d3543fa4cb9a198f4c202123af
nishaanntt <nparashar1998@gmail.com>
2025-07-28 20:24:40 +0530

    modal component

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
-import { Button, Image, Pressable, Text, View } from 'react-native';
-const logoImg = require('./assets/adaptive-icon.png');
+import { Button, Modal, Text, View } from 'react-native';
+
+import { useState } from 'react';
 
 export default function App() {
+	const [modalVisible, setModalVisible] = useState(false);
+
+	const onClosePress = () => {
+		setModalVisible(false);
+	};
+
 	return (
 		<View style={{ backgroundColor: 'plum', flex: 1, padding: 60 }}>
 			<Button
 				title='Press'
-				onPress={() => console.log('button pressed')}
+				onPress={() => setModalVisible(true)}
 				color='white'
 			/>
-			{/* ** PRESSABLE COMPONENT ** */}
-			<Pressable onPress={() => console.log('Image pressed')}>
-				<Image source={logoImg} style={{ height: 300, width: 300 }} />
-			</Pressable>
-			{/* ** PRESSABLE COMPONENT ** */}
-			<Pressable onPress={() => console.log('Text Pressed')}>
-				<Text>
-					Laborum fugiat aliqua cillum aute sit dolor quis. Deserunt magna ut
-					culpa anim dolore duis nulla tempor mollit nulla mollit. Occaecat anim
-					dolore duis anim laborum proident occaecat. Ullamco deserunt enim
-					velit sint enim anim in enim. Deserunt laboris id laborum ad non.In do
-					deserunt in ut et minim enim consequat id. Nostrud qui elit consequat
-					consectetur qui do aute cupidatat proident. Ex excepteur ipsum
-					pariatur do aliqua nisi adipisicing nulla deserunt consequat
-					exercitation pariatur veniam. Ut eu consequat amet amet labore ad enim
-					ut mollit fugiat ullamco. Ullamco consectetur ut consequat cillum elit
-					ullamco. Incididunt cillum ipsum tempor irure quis ea reprehenderit
-					cillum do anim velit ut et mollit. Quis voluptate eiusmod nostrud quis
-					culpa elit cillum laboris.
-				</Text>
-			</Pressable>
+			<Modal
+				visible={modalVisible}
+				onRequestClose={() => setModalVisible(false)}
+				animationType='slide'
+				// animationType='fade'
+				presentationStyle='pageSheet'
+				// presentationStyle='formSheet'
+				// formSheet provides a similar UI but differs on larger devices. formSheet is narrower, pageSheet is wider
+			>
+				<View style={{ backgroundColor: 'lightblue', flex: 1, padding: 60 }}>
+					<Text>Modal Content</Text>
+					<Button title='close' color='midnightblue' onPress={onClosePress} />
+				</View>
+			</Modal>
 		</View>
 	);
 }
