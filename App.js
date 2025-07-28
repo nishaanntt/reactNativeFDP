7c1f3142fd8f3519e5bf997a163cacd83c0cfeee
nishaanntt <nparashar1998@gmail.com>
2025-07-28 22:39:40 +0530

    flex layouts

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Box from './components/Box';
 
 const App = () => {
 	return (
 		<View style={styles.container}>
-			<View style={styles.darkMode}>
-				<Text style={styles.darkModeText}>Style Inheritance</Text>
-			</View>
-			<View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
-				{/* The box shadow property will only be visible in iOS */}
-				<Text>Lightblue Box</Text>
-			</View>
-			<View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-				<Text>Lightgreen Box</Text>
-			</View>
+			<Box style={{ backgroundColor: 'pink' }}>Box 1</Box>
+			<Box style={{ backgroundColor: 'plum' }}>Box 2</Box>
+			<Box style={{ backgroundColor: 'lightblue' }}>Box 3</Box>
+			<Box style={{ backgroundColor: 'lightgreen' }}>Box 4</Box>
+			<Box style={{ backgroundColor: 'skyblue' }}>Box 5</Box>
 		</View>
 	);
 };
@@ -22,43 +19,8 @@ export default App;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'plum',
-		padding: 60,
-	},
-	darkMode: {
-		backgroundColor: 'black',
-		color: 'white', //no change. does not support style Inheritance unlike web
-	},
-	darkModeText: {
-		color: 'white',
-	},
-	boxShadow: {
-		shadowColor: 'blue',
-		shadowOffset: {
-			width: 6,
-			height: 6,
-		},
-		shadowOpacity: 0.6,
-		shadowRadius: 4,
-	},
-	androidShadow: {
-		elevation: 10,
-	},
-	box: {
-		width: 250,
-		height: 250,
-		paddingVertical: 20,
-		paddingHorizontal: 10,
-		marginVertical: 25,
-		borderWidth: 2,
+		marginTop: 64,
+		borderWidth: 6,
 		borderColor: 'red',
-		borderStyle: 'solid',
-		borderRadius: 10,
-	},
-	lightblueBg: {
-		backgroundColor: 'lightblue',
-	},
-	lightgreenBg: {
-		backgroundColor: 'lightgreen',
 	},
 });
