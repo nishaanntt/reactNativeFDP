bd2f24d25c12ad540bbcb32b7d5536ff0ea7cdb5
nishaanntt <nparashar1998@gmail.com>
2025-07-28 20:36:51 +0530

    activity indicator

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar, View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={{ backgroundColor: 'plum', flex: 1, padding: 60 }}>
-			<StatusBar
-				backgroundColor='lightgreen' //only applicable on android
-				barStyle='light-content' //controls the text color on the status bar
-				// hidden //controls the visibility of the status bar
+		<View style={{ backgroundColor: 'pink', flex: 1, padding: 60 }}>
+			<ActivityIndicator />
+			<ActivityIndicator size='large' />
+			<ActivityIndicator size='large' color='green' />
+			<ActivityIndicator
+				size='large'
+				color='green'
+				animating={false} //controls visibility. Use state to toggle
 			/>
 		</View>
 	);
