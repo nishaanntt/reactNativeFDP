a414ffda02f47214903e048ca5ab62c519d12904
nishaanntt <nparashar1998@gmail.com>
2025-07-29 01:46:09 +0530

    platform api

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+// 1. Platform.OS
+// 2. Platform.select
+// 3. Platform specific extensions
+
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import CustomButton from './components/CustomButton/CustomButton';
 
 const App = () => {
 	return (
-		<SafeAreaView style={styles.safeContainer}>
-			<View style={styles.container}>
-				<View style={styles.box}>
-					<Text style={styles.text}>Welcome</Text>
-				</View>
+		// <SafeAreaView style={styles.safeContainer}>
+		<View style={styles.container}>
+			<View style={styles.box}>
+				<Text style={styles.text}>Welcome</Text>
 			</View>
-		</SafeAreaView>
+			{/* <CustomButton
+					onPress={() => console.log('ios button pressed')}
+					title='iOS Button'
+				/> */}
+		</View>
+		// </SafeAreaView>
 	);
 };
 
@@ -18,12 +28,24 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: 'plum',
+		// paddingTop: Platform.OS === 'android' ? 25 : 0,
 	},
 	box: {
 		padding: 20,
 	},
 	text: {
 		fontSize: 24,
+		// ...Platform.select({
+		// 	ios: {
+		// 		fontSize: 50,
+		// 		color: 'white',
+		// 		fontStyle: 'italic',
+		// 	},
+		// 	android: {
+		// 		fontSize: 24,
+		// 		color: 'purple',
+		// 	},
+		// }),
 		fontWeight: 'bold',
 		textAlign: 'center',
 	},
