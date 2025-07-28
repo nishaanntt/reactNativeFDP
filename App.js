446b6ee6ed73fe3a33be42bb9ccf2b36a2d16b5b
nishaanntt <nparashar1998@gmail.com>
2025-07-28 21:11:13 +0530

    box model

--- App.js
+++ App.js
@@ -6,8 +6,7 @@ const App = () => {
 			<View style={[styles.box, styles.lightblueBg]}>
 				<Text>Lightblue Box</Text>
 			</View>
-			<View
-				style={[styles.box, styles.lightgreenBg, { backgroundColor: 'pink' }]}>
+			<View style={[styles.box, styles.lightgreenBg]}>
 				<Text>Lightgreen Box</Text>
 			</View>
 		</View>
@@ -24,8 +23,20 @@ const styles = StyleSheet.create({
 	},
 	box: {
 		width: 100,
+		// width: '25%',
 		height: 100,
+		// height: '25%',
 		padding: 10,
+		// paddingVertical: 20,
+		// paddingHorizontal: 10,
+		margin: 10,
+		// marginHorizontal: 15,
+		// marginVertical: 25,
+		// border: '2px solid red'
+		borderWidth: 2,
+		borderColor: 'red',
+		borderStyle: 'solid',
+		borderRadius: 10,
 	},
 	lightblueBg: {
 		backgroundColor: 'lightblue',
