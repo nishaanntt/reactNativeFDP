97f99f5e890014fb0d3ef943d21690e129085129
nishaanntt <nparashar1998@gmail.com>
2025-07-28 19:45:47 +0530

    Core component - Text

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
+import { Text, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
 
 export default function App() {
-	return <View style={{ backgroundColor: 'pink', flex: 1 }}></View>;
+	return (
+		<View style={{ backgroundColor: 'pink', flex: 1, padding: 60 }}>
+			<Text style={{ fontWeight: 'bold' }}>
+				<Text style={{ color: 'white' }}>Hello</Text> World
+			</Text>
+		</View>
+	);
 }
