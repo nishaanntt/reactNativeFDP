03115a0910d6ef859e23ebc90091ce75089fb44e
nishaanntt <nparashar1998@gmail.com>
2025-07-28 20:42:22 +0530

    alert api

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import { ActivityIndicator, View } from 'react-native';
+import { Alert, Button, View } from 'react-native';
 
 export default function App() {
 	return (
 		<View style={{ backgroundColor: 'pink', flex: 1, padding: 60 }}>
-			<ActivityIndicator />
-			<ActivityIndicator size='large' />
-			<ActivityIndicator size='large' color='green' />
-			<ActivityIndicator
-				size='large'
-				color='green'
-				animating={false} //controls visibility. Use state to toggle
+			<Button title='Alert' onPress={() => Alert.alert('Invalid Data')} />
+			<Button
+				title='Alert 2'
+				onPress={() => Alert.alert('Alert Title', 'This is the alert message')}
+			/>
+			<Button
+				title='Alert 3'
+				onPress={() =>
+					Alert.alert('Alert Title', 'This is the alert message', [
+						{
+							text: 'ok',
+							onPress: () => console.log('ok pressed'),
+						},
+					])
+				}
+			/>
+			<Button
+				title='Alert 5'
+				onPress={() =>
+					Alert.alert('Alert Title', 'This is the alert message', [
+						{
+							text: 'button 1',
+							onPress: () => console.log('button 1 pressed'),
+						},
+						{
+							text: 'button 2',
+							onPress: () => console.log('button 2 pressed'),
+						},
+						{
+							text: 'button 3',
+							onPress: () => console.log('button 3 pressed'),
+						},
+						{
+							text: 'button 4',
+							onPress: () => console.log('button 4 pressed'),
+						},
+					])
+				}
 			/>
 		</View>
 	);
