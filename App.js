97cf49c012ee0dfd1c388c3ad40c4c21f57855a9
nishaanntt <nparashar1998@gmail.com>
2025-07-28 19:54:22 +0530

    View component

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-import { Text, View } from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 
 export default function App() {
-	return (
-		<View style={{ backgroundColor: 'pink', flex: 1, padding: 60 }}>
-			<Text style={{ fontWeight: 'bold' }}>
-				<Text style={{ color: 'white' }}>Hello</Text> World
-			</Text>
-		</View>
-	);
+	return <View style={{ backgroundColor: 'pink', flex: 1 }}></View>;
 }
