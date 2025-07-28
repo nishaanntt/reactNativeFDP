38c297f5cff03cfe361607d414b5524593ecda37
nishaanntt <nparashar1998@gmail.com>
2025-07-28 20:13:30 +0530

    pressable component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Button, View } from 'react-native';
+import { Button, Image, Pressable, Text, View } from 'react-native';
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
@@ -8,8 +8,28 @@ export default function App() {
 				title='Press'
 				onPress={() => console.log('button pressed')}
 				color='white'
-				disabled
 			/>
+			{/* ** PRESSABLE COMPONENT ** */}
+			<Pressable onPress={() => console.log('Image pressed')}>
+				<Image source={logoImg} style={{ height: 300, width: 300 }} />
+			</Pressable>
+			{/* ** PRESSABLE COMPONENT ** */}
+			<Pressable onPress={() => console.log('Text Pressed')}>
+				<Text>
+					Laborum fugiat aliqua cillum aute sit dolor quis. Deserunt magna ut
+					culpa anim dolore duis nulla tempor mollit nulla mollit. Occaecat anim
+					dolore duis anim laborum proident occaecat. Ullamco deserunt enim
+					velit sint enim anim in enim. Deserunt laboris id laborum ad non.In do
+					deserunt in ut et minim enim consequat id. Nostrud qui elit consequat
+					consectetur qui do aute cupidatat proident. Ex excepteur ipsum
+					pariatur do aliqua nisi adipisicing nulla deserunt consequat
+					exercitation pariatur veniam. Ut eu consequat amet amet labore ad enim
+					ut mollit fugiat ullamco. Ullamco consectetur ut consequat cillum elit
+					ullamco. Incididunt cillum ipsum tempor irure quis ea reprehenderit
+					cillum do anim velit ut et mollit. Quis voluptate eiusmod nostrud quis
+					culpa elit cillum laboris.
+				</Text>
+			</Pressable>
 		</View>
 	);
 }
