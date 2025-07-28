faecffc67040ab7bfc2642e52273d772f2c7c1a6
nishaanntt <nparashar1998@gmail.com>
2025-07-29 02:24:58 +0530

    rendering lists

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
+import pokemonList from './data.json';
 
-export default function App() {
+const App = () => {
 	return (
-		<View style={styles.container}>
-			<Text>Hello World!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<SafeAreaView style={styles.container}>
+			<ScrollView style={styles.scrollView}>
+				{pokemonList.map(pokemon => {
+					return (
+						<View style={styles.card} key={pokemon.id}>
+							<Text style={styles.cardText}>{pokemon.name}</Text>
+							<Text style={styles.cardText}>{pokemon.type}</Text>
+						</View>
+					);
+				})}
+			</ScrollView>
+		</SafeAreaView>
 	);
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: '#f5f5f5',
+		paddingTop: StatusBar.currentHeight, //safeareaview will not be affected by this
+	},
+	scrollView: {
+		paddingHorizontal: 16,
+	},
+	card: {
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		padding: 16,
+		borderRadius: 8,
+		borderWidth: 1,
+		marginBottom: 16,
+	},
+	cardText: {
+		fontSize: 24,
 	},
 });
