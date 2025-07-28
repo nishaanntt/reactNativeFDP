e3e01347875cd0dd9a686db532cd33ca9d8a0032
nishaanntt <nparashar1998@gmail.com>
2025-07-28 20:34:27 +0530

    status bar component

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
-import { Button, Modal, Text, View } from 'react-native';
-
-import { useState } from 'react';
+import { StatusBar, View } from 'react-native';
 
 export default function App() {
-	const [modalVisible, setModalVisible] = useState(false);
-
-	const onClosePress = () => {
-		setModalVisible(false);
-	};
-
 	return (
 		<View style={{ backgroundColor: 'plum', flex: 1, padding: 60 }}>
-			<Button
-				title='Press'
-				onPress={() => setModalVisible(true)}
-				color='white'
+			<StatusBar
+				backgroundColor='lightgreen' //only applicable on android
+				barStyle='light-content' //controls the text color on the status bar
+				// hidden //controls the visibility of the status bar
 			/>
-			<Modal
-				visible={modalVisible}
-				onRequestClose={() => setModalVisible(false)}
-				animationType='slide'
-				// animationType='fade'
-				presentationStyle='pageSheet'
-				// presentationStyle='formSheet'
-				// formSheet provides a similar UI but differs on larger devices. formSheet is narrower, pageSheet is wider
-			>
-				<View style={{ backgroundColor: 'lightblue', flex: 1, padding: 60 }}>
-					<Text>Modal Content</Text>
-					<Button title='close' color='midnightblue' onPress={onClosePress} />
-				</View>
-			</Modal>
 		</View>
 	);
 }
