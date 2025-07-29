786e21455b6bbe4377c4a5e2dd23a85c078f6664
nishaanntt <nparashar1998@gmail.com>
2025-07-29 07:14:36 +0530

    textinput

--- App.js
+++ App.js
@@ -1,91 +1,29 @@
-import {
-	FlatList,
-	SafeAreaView,
-	ScrollView,
-	StatusBar,
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native';
-
-import pokemonList from './data.json';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const App = () => {
+	const [name, setName] = useState('');
+	console.log('name: ', name);
 	return (
-		<SafeAreaView style={styles.container}>
-			{/* *** RENDERING LISTS WITH MAP *** */}
-
-			{/* <ScrollView style={styles.scrollView}>
-				{pokemonList.map(pokemon => {
-        console.log(pokemon.id)
-					return (
-						<View style={styles.card} key={pokemon.id}>
-							<Text style={styles.cardText}>{pokemon.name}</Text>
-							<Text style={styles.cardText}>{pokemon.type}</Text>
-						</View>
-					);
-				})}
-			</ScrollView> */}
-
-			{/* *** FLATLIST *** */}
-
-			{/* <View style={styles.scrollView}>
-				<FlatList
-					data={pokemonList}
-					renderItem={({ item }) => {
-						console.log(item.id);
-						return (
-							<View style={styles.card} key={item.id}>
-								<Text style={styles.cardText}>{item.name}</Text>
-								<Text style={styles.cardText}>{item.type}</Text>
-							</View>
-						);
+		<SafeAreaView style={{ backgroundColor: 'pink', flex: 1 }}>
+			<View style={{ paddingHorizontal: 16 }}>
+				<TextInput
+					style={{
+						backgroundColor: '#ddd',
+						padding: 10,
+						margin: 12,
+						color: '#fff',
 					}}
-					// keyExtractor={(item, index) => item.id.toString()}
-					// ItemSeparatorComponent={<View style={{ height: 16 }} />}
-					// horizontal={true}
-					// ListEmptyComponent={<Text>No Items Found</Text>}
-					// ListHeaderComponent={
-					// 	<Text style={styles.headerText}>Pokemon List</Text>
-					// }
-					// ListFooterComponent={
-					// 	<Text style={styles.footerText}>End of List</Text>
-					// }
+					placeholder='Enter Name'
+					value={name}
+					onChangeText={setName}
 				/>
-			</View> */}
+				<Text style={{ fontSize: 24 }}>Hi, my name is {name}</Text>
+			</View>
 		</SafeAreaView>
 	);
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#f5f5f5',
-		paddingTop: StatusBar.currentHeight, //safeareaview will not be affected by this
-	},
-	scrollView: {
-		paddingHorizontal: 16,
-	},
-	card: {
-		backgroundColor: '#fff',
-		padding: 16,
-		borderRadius: 8,
-		borderWidth: 1,
-		marginBottom: 16,
-	},
-	cardText: {
-		fontSize: 24,
-	},
-	headerText: {
-		fontSize: 24,
-		textAlign: 'center',
-		marginBottom: 12,
-	},
-	footerText: {
-		fontSize: 24,
-		textAlign: 'center',
-		marginTop: 12,
-	},
-});
+const styles = StyleSheet.create({});
